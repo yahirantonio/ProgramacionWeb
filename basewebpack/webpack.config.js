94ca9be0f80e24226513a4c3ff209ab2b07a00a6
yahirantonio <yahirantoni90@gmail.com>
2021-05-25 12:48:04 -0500

    Base webpack
    Aquí están las configuraciones para tener la base de webpack, configuraciones de html, css y js

---
+++ basewebpack/webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.js",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules:[
+    {
+      test:/\.js$/i,
+      exclude: /node_modules/,
+      use:"babel-loader",
+    },
+    {
+      test:/\.html/i,
+      use:[
+      {
+        loader: "html-loader",
+        options: {
+          minimize: true,
+        },
+      },
+      ],
+    },
+    {
+      test:/\.css$/i,
+      use:[MiniCssExtractPlugin.loader, "css-loader"],
+    },
+    ],
+  },
+  plugins:[
+   new HtmlWebpackPlugin({
+    filename: "./index.html",
+    template: "./src/index.html",
+   }),
+   new MiniCssExtractPlugin(),
+  ],
+};
